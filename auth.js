edb171ea7eb45673973b82e704b07fe91af91db5
tranvy506 <161282400+tranvy506@users.noreply.github.com>
2026-09-19 11:20:25 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -28,7 +28,7 @@ function login(username, password) {
   }
 
   // Đăng nhập hợp lệ: admin / 123
-  if (trimmedUser === 'admin' && trimmedPass === '1234') {
+  if (trimmedUser === 'admin' && trimmedPass === '9999') {
     return true;
   }
 
